4ab4da6e9168e4bf1e37b28d5b5af6d40852af37
henrique <henripereira922@gmail.com>
2025-10-14 20:41:01 -0300

    Empresa feita

--- src/main/java/com/spotfy/spotfyproject/Repository/EmpresaRepository.java
+++ src/main/java/com/spotfy/spotfyproject/Repository/EmpresaRepository.java
@@ -1,4 +1,22 @@
 package com.spotfy.spotfyproject.Repository;
 
-public class EmpresaRepository {
+import com.gerenciamento.estoque.Model.EmpresaModel;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EmpresaRepository extends JpaRepository<EmpresaModel,Integer> {
+    Optional<EmpresaModel> findByCdEmpresa(Integer cdEmpresa);
+    Optional<EmpresaModel> findByNuCnpj(String nuCnpj);
+    @Query("SELECT emp FROM EmpresaModel emp WHERE emp.flAtivo = 'S' ")
+    List<EmpresaModel> findAllByFlAtivo();
+    @Transactional
+    Optional<EmpresaModel> deleteByCdEmpresa(Integer cdEmpresa);
+
+
+
+
 }
